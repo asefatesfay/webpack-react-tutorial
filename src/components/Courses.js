f6036208eaf16c25112d52a8b88f9dd1f2bcd4f2
Assefa Tesfay <x3p8@nordstrom.net>
2019-07-02 15:22:30 -0700

    update css

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route, Link} from 'react-router-dom';
+import styles from "./courses.css";
 class Courses extends React.Component {
   render () {
     const {match} = this.props;
     return (
-      <div>
-      <ul>
-        <li><Link to={`${match.url}/technology`}>Technology</Link></li>
-        <li><Link to={`${match.url}/business`}>Business</Link></li>
-        <li><Link to={`${match.url}/economics`}>Economics</Link></li>
+      <div className={styles.courses}>
+      <ul className="list-group">
+        <li className="list-group-item list-group-item-success"><Link to={`${match.url}/technology`}>Technology</Link></li>
+        <li className="list-group-item list-group-item-secondary"><Link to={`${match.url}/business`}>Business</Link></li>
+        <li className="list-group-item list-group-item-info"><Link to={`${match.url}/economics`}>Economics</Link></li>
       </ul>
   
       <Route exact path={`${match.path}/:course`} render={({match})=>(<div>This is {match.params.course}</div>)} />
